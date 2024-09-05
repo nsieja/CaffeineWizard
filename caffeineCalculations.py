892216f12254f6ba8aec6850b8dd1ebc66adeda2
Nick Sieja <nsieja@gmail.com>
2024-09-05 11:39:24 -1000

    Created file caffeineCalculations.py and imported numpy to setup framework for calculating the body response to caffeine intake.

---
+++ caffeineCalculations.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env
+
+"""
+File:        caffeineCalculations.py
+Author:      Nicholas Sieja
+Date:        9/5/2024
+Description: Accepts user input for caffeine consumption, assumes or accepts input for function constants, calculates bodily response to caffeine.
+"""
+
+import numpy as np
+
