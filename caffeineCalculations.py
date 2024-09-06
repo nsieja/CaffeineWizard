e9ddb9f6e8af83e85fee15d02ab2f3a94c6d86a0
Nick Sieja <nsieja@gmail.com>
2024-09-05 15:45:41 -1000

    created function odes to solve system of odes that define caffeine absorption and removal
    updated the docstring of cafCalculate to match
    formatting of new function odes

--- caffeineCalculations.py
+++ caffeineCalculations.py
@@ -19,28 +19,52 @@ def cafCalculate(caffeine, drinkTime, projectionTime):
     two returned lists will be passed to the plotting 
     function to generate a graph(s) for the user
 
-            Parameters:
-                    caffeine (int): The amount of caffeine 
-                    consumed in mg
+    Parameters:
+    caffeine      : (int) amount of caffeine consumed (mg)
+    drinkTime     : (int) time to consume caffeine (min)
+    projectionTime: (int) time to project out response (min)
 
-                    drinkTime (int): The amount of time 
-                    taken to consume the caffeine in minutes
+    Returns:
+    cafModel: (tuple) contains 1 list of floats 
+    corresponding to caffeine levels in GI tract, 
+    1 list of floats corresponding to caffeine levels in 
+    blood, and 1 list of floats that represent the time 
+    (independent variable) for plotting purposes
 
-                    projectionTime (int): The amount of time 
-                    over which to calculate and project the 
-                    response in minutes
-
-            Returns:
-                    cafModel (tuple): tuple containing 1 
-                    list of floats corresponding to 
-                    caffeine levels in GI tract, 1 list of 
-                    floats corresponding to caffeine levels 
-                    in blood, and 1 list of floats that 
-                    represent the time (independent variable) 
-                    for plotting purposes
-
-            Precondition:
-                    caffeine is type int, drinkTime is type 
-                    int, projectionTime is type int
+    Precondition:
+    caffeine is int, drinkTime is int, projectionTime is int
     '''
     pass
+
+def odes(q,y,t,k1,k2,x0,T):
+    """
+    Compute the derivatives of q and y for the system of ODEs:
+    
+        dq/dt = -k1 * q + x(t)
+        dy/dt = k1 * q - k2 * y
+    
+    where x(t) describes the caffeine intake
+    
+    Parameters:
+    q : (float) current value of q(t)
+    y : (float) current value of y(t)
+    t : (float) current time that derivatives are being evaluated
+    k1: (float) constant rate coefficient for the first ODE
+    k2: (float) constant rate coefficient for the second ODE.
+    x0: (float) constant value of x(t) for t <= T
+    T : (float) time point at which x(t) is 0 (done consuming)
+
+    Returns:
+    [dqdt, dydt]: (list) contains the derivatives [dq/dt, dy/dt] 
+    at the current time point t
+    """
+    if t <= T:
+        x_t = x0    # Amt affeine over time T to consume
+    else:
+        x_t = 0     # Done consuming caffeine
+
+    # Define system of ODEs
+    dqdt = -k1*q + x_t      # GI tract kinetics
+    dydt = k1*q - k2*y      # Bloodstream kinetics
+    
+    return [dqdt, dydt]
